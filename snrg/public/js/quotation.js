e5709aaa7016bbb1f2cf8c5d48340ebfb3f20452
Nikhil Goel <nikhil@Nikhils-MacBook-Air.local>
2026-08-29 14:21:14 +0530

    Fix pricing context for prefilled quotations

--- snrg/public/js/quotation.js
+++ snrg/public/js/quotation.js
@@ -1,5 +1,18 @@
 frappe.ui.form.on("Quotation", {
-    refresh:function(frm){
+    onload(frm) {
+        // List filters are copied directly into new documents without firing
+        // the party_name event. Fetch the full Customer context so pricing
+        // rules use the correct Customer Group and Territory.
+        if (
+            frm.is_new() &&
+            frm.doc.quotation_to === "Customer" &&
+            frm.doc.party_name
+        ) {
+            frm.trigger("party_name");
+        }
+    },
+
+    refresh(frm) {
         frm.set_query("transporter", {
             filters: {
                 is_transporter: 1
@@ -13,4 +26,4 @@ frappe.ui.form.on("Quotation", {
     //         },
     //     });
     // }
-})
+});
